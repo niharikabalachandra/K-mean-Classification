a3cbc0454ff74859bc20b5c9d71f24a55bd5e4f9
niharikabalachandra <niharikabalachandra@gmail.com>
2017-03-14 11:51:55 -0500

    Update Kmeans_LOOCV.R

--- Kmeans_LOOCV.R
+++ Kmeans_LOOCV.R
@@ -1,21 +1,4 @@
-##Problem Statement: 
-##The two outcomes for the given input data "germancredit.csv," 
-##are success and failure, which represent defaulting on the loan and not defaulting, respectively. 
-##You need to perform logistic regression by considering the features of the loan and 
-##characteristics of the borrower as the explanatory variables.
-
-##After this, you are required to perform k-means classification with the help of three 
-##continuous variables-duration, amount, and instalment.
-
-##Finally, take help of the cross-validation method to determine the % of identification 
-##that happens correctly with k = 5 nearest neighbors.
-
-
-
-#Solution
-
-
-##Dataset considered is the German Credit Data with data on 1000 loans
+##Dataset considered is the German Credit Data (germancredit_info.csv) with data on 1000 loans. 
 
 library(textir) ## needed to standardize the data
 library(class) ## needed for knn
